8c223fcb27daecf49a59f106a902f6f32f952be9
nathanpasdutout <nathanbaudrier@gmail.com>
2026-03-27 15:39:13 +0100

    add nav bar component

---
+++ src/app/core/nav-bar/nav-bar.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  imports: [],
+  templateUrl: './nav-bar.html',
+  styleUrl: './nav-bar.scss',
+})
+export class NavBar {
+  protected readonly languages: string[] = [
+    // Temporary
+    'Français',
+    'Anglais',
+  ];
+}
